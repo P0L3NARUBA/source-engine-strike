314788babd34e5c4ed5284f601a689c8b3053602
yungDoom <cagataybugrafan@gmail.com>
2025-02-13 20:46:30 +0300

    Fixed Achievements

--- gameui/BasePanel.cpp
+++ gameui/BasePanel.cpp
@@ -2158,15 +2158,17 @@ void CBasePanel::RunMenuCommand(const char *command)
 
     else if ( !Q_stricmp( command, "OpenCSAchievementsDialog" ) )
     {
+
         if ( IsPC() )
         {
+#ifndef NO_STEAM
             if ( !steamapicontext->SteamUser() || !steamapicontext->SteamUser()->BLoggedOn() )
             {
                 vgui::MessageBox *pMessageBox = new vgui::MessageBox("#GameUI_Achievements_SteamRequired_Title", "#GameUI_Achievements_SteamRequired_Message", this );
                 pMessageBox->DoModal();
                 return;
             }
-
+#endif
 			OnOpenCSAchievementsDialog();
         }
     }
@@ -3406,21 +3408,10 @@ void CBasePanel::OnOpenCSAchievementsDialog()
 		int screenHeight = 0;
 		engine->GetScreenSize( screenWide, screenHeight );
 
-		// [smessick] For lower resolutions, open the Steam achievements instead of the CSS achievements screen.
-		if ( screenWide < GameClientExports()->GetAchievementsPanelMinWidth() )
-		{
-			ISteamFriends *friends = steamapicontext->SteamFriends();
-			if ( friends )
-			{
-				friends->ActivateGameOverlay( "Achievements" );
-			}
-		}
-		else
-		{
 			// Display the CSS achievements screen.
 			GameClientExports()->CreateAchievementsPanel( this );
 			GameClientExports()->DisplayAchievementPanel();
-		}
+		
     }
 }
 
